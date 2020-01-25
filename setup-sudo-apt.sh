94cb6ae4bf542aa4050e4badcff2e8978609213a
Koji Ishiwatari <koji.ishiwatari@gmail.com>
2020-01-25 19:50:08 +0900

    Add script files.

---
+++ setup-sudo-apt.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+### 更新
+sudo apt -y update
+sudo apt -y upgrade
+### 必要なアプリのインストール
+sudo apt -y install git git-lfs fdclone emacs net-tools apt-file
+### 定義ファイルの更新
+sudo apt-file update
