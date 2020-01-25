c1969d3c2bb822a2042e0eba97c287c430ad605c
Koji Ishiwatari <koji.ishiwatari@gmail.com>
2020-01-25 19:27:20 +0900

    Fix typo.

--- setup-root-ssh-firewall-japan.sh
+++ setup-root-ssh-firewall-japan.sh
@@ -3,6 +3,8 @@
 ### ホワイトリスト作成
 ipset destroy white_list_japan
 ipset create white_list_japan hash:net
+cp -p jp.txt jp.txt.bkup
+curl https://ipv4.fetus.jp/jp.txt > jp.txt
 curl https://ipv4.fetus.jp/jp.txt | grep -v '^#' | xargs -I@ -P 0  ipset add white_list_japan @
 ipset add white_list_japan 192.168.0.0/16
 ipset add white_list_japan 10.0.0.0/8
@@ -10,7 +12,8 @@ ipset add white_list_japan 172.16.0.0/12
 ipset list white_list_japan
 ipset save white_list_japan > white_list_japan.txt
 iptables -I INPUT -m state --state NEW -p tcp --dport 22 -m set --match-set white_list_japan src -j ACCEPT
-sh -c 'ipset save > /etc/ipset.conf'
+ipset save > /etc/ipset.conf
+iptables-save > /etc/iptables.up.rules
 ### ファイル作成
 cat <<EOF > /etc/network/if-pre-up.d/iptables-with-ipset
 #!/bin/sh
@@ -21,7 +24,7 @@ cat <<EOF > /etc/network/if-pre-up.d/iptables-with-ipset
 ### read data from file list
 ipset restore < /etc/ipset.conf
 ### 
-iptables-restore < /etc/iptales.up.rules" >> /etc/network/if-pre-up.d/iptables-with-ipset
+iptables-restore < /etc/iptales.up.rules
 EOF
 ### 実行属性追加
 chmod ug+x /etc/network/if-pre-up.d/iptables-with-ipset
--- setup-ssh-firewall-japan.sh
+++ setup-ssh-firewall-japan.sh
@@ -3,6 +3,8 @@
 ### ホワイトリスト作成
 sudo ipset destroy white_list_japan
 sudo ipset create white_list_japan hash:net
+cp -p jp.txt jp.txt.bkup
+curl https://ipv4.fetus.jp/jp.txt > jp.txt
 curl https://ipv4.fetus.jp/jp.txt | grep -v '^#' | xargs -I@ -P 0 sudo ipset add white_list_japan @
 sudo ipset add white_list_japan 192.168.0.0/16
 sudo ipset add white_list_japan 10.0.0.0/8
@@ -11,6 +13,7 @@ sudo ipset list white_list_japan
 sudo ipset save white_list_japan > white_list_japan.txt
 sudo iptables -I INPUT -m state --state NEW -p tcp --dport 22 -m set --match-set white_list_japan src -j ACCEPT
 sudo sh -c 'ipset save > /etc/ipset.conf'
+sudo sh -c 'iptables-save > /etc/iptables.up.rules'
 ### ファイル作成
 sudo cat <<EOF > /etc/network/if-pre-up.d/iptables-with-ipset
 #!/bin/sh
@@ -21,7 +24,7 @@ sudo cat <<EOF > /etc/network/if-pre-up.d/iptables-with-ipset
 ### read data from file list
 ipset restore < /etc/ipset.conf
 ### 
-iptables-restore < /etc/iptales.up.rules" >> /etc/network/if-pre-up.d/iptables-with-ipset
+iptables-restore < /etc/iptales.up.rules
 EOF
 ### 実行属性追加
 sudo chmod ug+x /etc/network/if-pre-up.d/iptables-with-ipset
